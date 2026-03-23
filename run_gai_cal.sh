9fd2be96f7b46af29fd164e7652e3435f5a5cf1f
David Selby <david.selby@dfki.de>
2026-03-23 17:08:06 +0100

    Initial commit

---
+++ run_gai_cal.sh
@@ -0,0 +1,9 @@
+python gai_cal.py \
+    data/processed/AGP/meta.tsv \
+    data/processed/AGP/otu.tsv \
+    my_output/AGP-agefix
+
+python gai_cal.py \
+    data/processed/GGMP/meta.tsv \
+    data/processed/GGMP/otu.tsv \
+    my_output/GGMP-agefix
